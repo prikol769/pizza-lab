8335f03eb983b6d52001b110d8cc9d60f4a22002
Mykyta Krumalenko <prikol769@gmail.com>
2024-07-22 16:54:44 -0500

    addded Header

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  const style = {
+    color: "red",
+    fontSize: "48px",
+  };
+  return (
+    <header className="header">
+      <h1 style={style}>Fast React Pizza</h1>
+    </header>
+  );
+};
+
+export default Header;
